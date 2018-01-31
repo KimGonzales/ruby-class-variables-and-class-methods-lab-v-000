a129d638d368fdb5c3742fd260d52c3839f49521
Kim Gonzales <kimgo589@gmail.com>
2018-01-31 22:05:16 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+class Song
+
+  attr_accessor :name, :genre, :artist
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name= name
+    @artist= artist
+    @genre = genre
+    @@count += 1
+  end
+
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    artists << artist
+  end
+end
